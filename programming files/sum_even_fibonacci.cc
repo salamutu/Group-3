93f30a10157c94cbc1d5ba45e9e0c713b1f28f86
Charlie Shapiro <110629696+cshapiro17@users.noreply.github.com>
2023-10-24 10:26:22 -0400

    Update sum_even_fibonacci.cc

--- programming files/sum_even_fibonacci.cc
+++ programming files/sum_even_fibonacci.cc
@@ -2,16 +2,22 @@
 
 int main()
 {
+    // initialize variables
     int a = 0, b = 1;
     int sum_even = 0;
     while (b < 4000000) {
+
+        // check to see if value is even
         if (b % 2 == 0) {
+            // if even theen add to sum of the even numbers
             sum_even += b;
         }
+        // modify variables accordingly
         int temp = b;
         b = a + b;
         a = temp;
     }
+    // display results
     std::cout << sum_even << std::endl;
 
     return 0;
