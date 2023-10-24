8b2e08b185f3cf923f61a825a87c182456100d5a
njblanch <122926164+njblanch@users.noreply.github.com>
2023-10-24 10:28:13 -0400

    Added comments to remove_duplicates.cc

--- programming files/remove_duplicates.cc
+++ programming files/remove_duplicates.cc
@@ -3,15 +3,20 @@
 #include <algorithm>
 using namespace std;
 
+// Function remove_duplicates sorts a vector and returns a vector with duplicates removed
 vector<int> remove_duplicates(vector<int> arr, vector<int>& removed_elements){
+    // Create vector to store unique elements
     vector<int> unique_arr;
+    // Sort vector
     sort(arr.begin(), arr.end());
     int n = arr.size();
+    // Loop through all elements in arr
     for(int i=0; i<n; i++){
+        // Add an element from arr to unique_arr if the value at index i is not equal to the value at index i-1 (not a duplicate)
         if(i == 0 || arr[i] != arr[i-1]){
             unique_arr.push_back(arr[i]);
         }
-        else{
+        else{    // Add the element to removed_elements if it is equal to the value at index i-1 (duplicate)
             removed_elements.push_back(arr[i]);
         }
     }
