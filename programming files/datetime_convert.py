c124a6c67927d4f4f7c3c3102d2d7797d3ba80c6
lbaileycoop <123046408+lbaileycoop@users.noreply.github.com>
2023-10-24 10:26:05 -0400

    Update datetime_convert.py
    added comments to the file

--- programming files/datetime_convert.py
+++ programming files/datetime_convert.py
@@ -1,7 +1,13 @@
 from datetime import datetime
 
+#creating a string variable to store a string value for the date and time 
 date_str = "2022-03-17 10:45:30"
+
+#creating an object variable that stores and converts time into a string by using the strptime function
 date_obj = datetime.strptime(date_str, '%Y-%m-%d %H:%M:%S')
+
+#creating a new variable that converts the value stored in date_obj into a string by using the strptime function 
 formatted_date = date_obj.strftime('%m/%d/%Y %H:%M:%S')
 
+#printing formatted date
 print(formatted_date)
